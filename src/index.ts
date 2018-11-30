437447a9a36ef57e46bc1887664316f149ee5ea5
ctare <sankaku900622@yahoo.co.jp>
2018-12-01 04:53:17 +0900

    simple command

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,21 @@ export const activate = (oni: Oni.Plugin.Api): void => {
     // We'll create a simple status bar item
     alert("hello world")
 
+    oni.commands.registerCommand(new Command(
+        "sample.hello",
+        "Sample Command",
+        "this is sample command",
+        () => {
+            alert("hello!!!!!")
+        }
+    ))
+}
+
+class Command implements Oni.Commands.ICommand {
+    constructor(
+        public command: string,
+        public name: string,
+        public detail: string,
+        public execute: Oni.Commands.CommandCallback,
+    ) {}
 }
